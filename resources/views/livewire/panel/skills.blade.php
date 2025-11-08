c8c36912119657f38bf0c06b32cff1898ca56410
Osama Abdelrahman <osbusiness662@gmail.com>
2025-11-08 18:11:52 +0000

    Build skills page main view

--- resources/views/livewire/panel/skills.blade.php
+++ resources/views/livewire/panel/skills.blade.php
@@ -1,3 +1,133 @@
-<div>
-    Yes I'm
+<div class="py-5">
+
+    {{-- Import Mesages --}}
+    <x-alerts.session></x-alerts.session>
+
+    {{-- Import Table --}}
+    <x-tables.search>
+        <x-slot:add>{{ '#skills-add-modal' }}</x-slot:add>
+        <x-slot:title>{{ __('Skills Management Table') }}</x-slot:title>
+        <x-slot:thead>
+            <th>{{ __('#') }}</th>
+            <th>{{ __('Name') }}</th>
+            <th>{{ __('Percentage') }}</th>
+            <th>{{ __('Level') }}</th>
+            <th>{{ __('Added') }}</th>
+            <th>{{ __('Updated') }}</th>
+            <th>{{ __('Action') }}</th>
+        </x-slot:thead>
+        <x-slot:tbody>
+            @if (count($skills) > 0)
+                @foreach ($skills as $i => $skill)
+                    <tr>
+                        <td scope="row">{{ $i += 1 }}</td>
+                        <td><img class="s-50" src="{{ asset('imgs/social/' . $skill->name . '.png') }}"
+                                alt="{{ $skill->name }}"></td>
+                        <td>{{ $skill->name }}</td>
+                        <td><a href="{{ $skill->url }}" target="_blank"
+                                rel="noopener noreferrer">{{ $skill->url }}</a></td>
+                        <td>{{ $skill->status }}</td>
+                        <td>{{ $skill->created_at->diffForHumans() }}</td>
+                        <td>{{ $skill->updated_at->diffForHumans() }}</td>
+                        <td>
+                            <button type="button" class="btn" data-bs-toggle="modal" title="Edit"
+                                data-bs-target="#social-edit-modal"
+                                wire:click.prevent="setSocialID({{ $skill->id }})">
+                                <i class="fas fa-edit text-primary"></i>
+                            </button>
+                            <button type="button" class="btn" data-bs-toggle="modal" title="Delete"
+                                data-bs-target="#deleteModal" wire:click.prevent="setSocialID({{ $skill->id }})">
+                                <i class="fas fa-trash text-danger"></i>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td colspan="8">
+                        <div class="alert alert-primary text-center" role="alert">
+                            Their is no <strong>Skills</strong> data saved yet!
+                        </div>
+                    </td>
+                </tr>
+            @endif
+        </x-slot:tbody>
+    </x-tables.search>
+
+    {{-- Import Adding Modal --}}
+    <x-modal.def>
+        <x-slot:id>{{ 'skills-add-modal' }}</x-slot:id>
+        <x-slot:title>{{ __('Adding new skills') }}</x-slot:title>
+        <form>
+            {{-- Skill Name --}}
+            <x-forms.input>
+                <x-slot:for>{{ 'name' }}</x-slot:for>
+                <x-slot:placeholder>{{ 'Ex: PHP | Laravel | HTML' }}</x-slot:placeholder>
+            </x-forms.input>
+            {{-- Skill Percentage --}}
+            <x-forms.input>
+                <x-slot:type>{{ 'number' }}</x-slot:type>
+                <x-slot:for>{{ 'percentage' }}</x-slot:for>
+                <x-slot:placeholder>{{ '50 <-> 100 [Only numbers]' }}</x-slot:placeholder>
+            </x-forms.input>
+            {{-- Skills Level --}}
+            <x-forms.radio>
+                <x-slot:for>{{ 'level' }}</x-slot:for>
+                <x-slot:title>{{ __('Skill Level') }}</x-slot:title>
+                <x-slot:values>{{ 'Beginner,Intermediate,Expert' }}</x-slot:values>
+            </x-forms.radio>
+            {{-- Submit --}}
+            <div class="text-end">
+                <button type="submit" class="btn btn-outline-primary s-100">
+                    <i class="fas fa-save me-1"></i>{{ __('Save') }}
+                </button>
+            </div>
+        </form>
+    </x-modal.def>
+
+    {{-- Import Editing Modal --}}
+    <x-modal.def>
+        <x-slot:id>{{ 'skills-edit-modal' }}</x-slot:id>
+        <x-slot:title>{{ __('Updating old skills') }}</x-slot:title>
+        <form>
+            {{-- Skill ID --}}
+            <x-forms.input>
+                <x-slot:type>{{ 'text' }}</x-slot:type>
+                <x-slot:for>{{ 'name' }}</x-slot:for>
+                <x-slot:placeholder>{{ 'Ex: PHP | Laravel | HTML' }}</x-slot:placeholder>
+            </x-forms.input>
+            {{-- Skill Name --}}
+            <x-forms.input>
+                <x-slot:for>{{ 'name' }}</x-slot:for>
+                <x-slot:placeholder>{{ 'Ex: PHP | Laravel | HTML' }}</x-slot:placeholder>
+            </x-forms.input>
+            {{-- Skill Percentage --}}
+            <x-forms.input>
+                <x-slot:type>{{ 'number' }}</x-slot:type>
+                <x-slot:for>{{ 'percentage' }}</x-slot:for>
+                <x-slot:placeholder>{{ '50 <-> 100 [Only numbers]' }}</x-slot:placeholder>
+            </x-forms.input>
+            {{-- Skills Level --}}
+            <x-forms.radio>
+                <x-slot:for>{{ 'level' }}</x-slot:for>
+                <x-slot:title>{{ __('Skill Level') }}</x-slot:title>
+                <x-slot:values>{{ 'Beginner,Intermediate,Expert' }}</x-slot:values>
+            </x-forms.radio>
+            {{-- Submit --}}
+            <div class="text-end">
+                <button type="submit" class="btn btn-outline-primary s-100">
+                    <i class="fas fa-save me-1"></i>{{ __('Update') }}
+                </button>
+                <button type="button" wire:click="cancel" class="btn btn-outline-danger s-100">
+                    <i class="fas fa-times me-1"></i>{{ __('Cancel') }}
+                </button>
+            </div>
+        </form>
+    </x-modal.def>
+
+    {{-- Import Confirming Modal --}}
+    <x-modal.confirm>
+        <x-slot:method>{{ 'delete' }}</x-slot:method>
+    </x-modal.confirm>
+
 </div>
