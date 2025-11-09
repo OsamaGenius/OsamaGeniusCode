fddc08560f3ecef0128e0cab981644a76d86c0f2
Osama Abdelrahman <osbusiness662@gmail.com>
2025-11-09 16:00:26 +0000

    Make radio component id dynamic

--- resources/views/components/forms/radio.blade.php
+++ resources/views/components/forms/radio.blade.php
@@ -5,8 +5,8 @@
     @endphp
     @foreach ($radios as $i => $val)
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" wire:model.defer="{{$for}}" id="option{{$i}}" value="{{$val}}">
-            <label class="form-check-label" for="option{{$i}}">{{$val}}</label>
+            <input class="form-check-input" type="radio" wire:model.defer="{{$for}}" id="{{$for . '_' . $i}}" value="{{$val}}">
+            <label class="form-check-label" for="{{$for . '_' . $i}}">{{$val}}</label>
         </div>
     @endforeach
     @error("$for")
