7ceb4b47d5d2aa107b819f23af30a1e45d843e67
Osama Abdelrahman <osbusiness662@gmail.com>
2025-11-09 18:37:12 +0000

    Apply Skills management at the public view

--- app/Livewire/Demo/Skills.php
+++ app/Livewire/Demo/Skills.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire\Demo;
 
+use App\Models\Skill;
 use Livewire\Component;
 
 class Skills extends Component
 {
     public function render()
     {
-        return view('livewire.demo.skills');
+        return view(
+            'livewire.demo.skills',
+            [
+                'fLinks' => Skill::where('type', 'Frontend')->get(['name', 'percentage']),
+                'bLinks' => Skill::where('type', 'Backend')->get(['name', 'percentage']),
+            ]
+        );
     }
 }
--- resources/views/livewire/demo/skills.blade.php
+++ resources/views/livewire/demo/skills.blade.php
@@ -1,98 +1,74 @@
-<section class="skills py-5 fade-sections" data-animate="fillProgressBars">
-    <div class="container">
-        <!-- Section Title -->
-        <div class="title text-center mb-5">
-            <h3 class="mb-3">Rating - <span>My Career Skills</span></h3>
-            <div class="decoration">
-                <div class="upline"></div>
-                <div class="downline"></div>
-                <div class="icon"></div>
-            </div>
-        </div>
+<div>
+    @if (count($fLinks) > 0 || count($bLinks) > 0)
+        <section class="skills py-5 fade-sections" data-animate="fillProgressBars">
+            <div class="container">
+                <!-- Section Title -->
+                <div class="title text-center mb-5">
+                    <h3 class="mb-3">Rating - <span>My Career Skills</span></h3>
+                    <div class="decoration">
+                        <div class="upline"></div>
+                        <div class="downline"></div>
+                        <div class="icon"></div>
+                    </div>
+                </div>
 
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-            <!-- Frontend Skills -->
-            <div class="col">
-                <h4 class="mb-4 text-start text-md-center">
-                    <i class="fas fa-file me-3"></i>
-                    Frontend Skills
-                </h4>
-                <!-- HTML Skill -->
-                <div class="position-relative">
-                    <label class="progress-label">90</label>
-                    <div class="progress mb-3">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
-                            aria-valuemax="100">
-                            <span class="pe-1 text-end">HTML</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- CSS Skill -->
-                <div class="position-relative">
-                    <label class="progress-label">95</label>
-                    <div class="progress mb-3">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0"
-                            aria-valuemax="100">
-                            <span class="pe-1 text-end">CSS</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- JavaScript Skill -->
-                <div class="position-relative">
-                    <label class="progress-label">75</label>
-                    <div class="progress mb-3">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
-                            aria-valuemax="100">
-                            <span class="pe-1 text-end">JavaScript</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!---->
+                <div class="row row-cols-1 row-cols-md-2 g-4">
 
-            <!-- Backend Skills -->
-            <div class="col">
-                <h4 class="mb-4 text-start text-md-center">
-                    <i class="fas fa-code me-3"></i>
-                    Backend Skills
-                </h4>
-                <!-- PHP Skill -->
-                <div class="position-relative">
-                    <label class="progress-label">80</label>
-                    <div class="progress mb-3">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0"
-                            aria-valuemax="100">
-                            <span class="pe-1 text-end">PHP</span>
+                    <!-- Frontend Skills -->
+                    @if (count($fLinks) > 0)
+                        <div class="col">
+
+                            <h4 class="mb-4 text-start text-md-center">
+                                <i class="fas fa-file me-3"></i>
+                                Frontend Skills
+                            </h4>
+
+                            @foreach ($fLinks as $link)
+                                <div class="position-relative">
+                                    <label class="progress-label">{{ $link->percentage }}</label>
+                                    <div class="progress mb-3">
+                                        <div class="progress-bar" role="progressbar"
+                                            aria-valuenow="{{ $link->percentage }}" aria-valuemin="0"
+                                            aria-valuemax="100">
+                                            <span class="pe-1 text-end">{{ $link->name }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+
                         </div>
-                    </div>
-                </div>
-                <!---->
-                <!-- Java Skill -->
-                <div class="position-relative">
-                    <label class="progress-label">85</label>
-                    <div class="progress mb-3">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0"
-                            aria-valuemax="100">
-                            <span class="pe-1 text-end">Java</span>
+                    @endif
+
+                    <!-- Backend Skills -->
+                    @if (count($bLinks) > 0)
+                        <div class="col">
+
+                            <h4 class="mb-4 text-start text-md-center">
+                                <i class="fas fa-file me-3"></i>
+                                Backend Skills
+                            </h4>
+
+                            @foreach ($bLinks as $link)
+                                <div class="position-relative">
+                                    <label class="progress-label">{{ $link->percentage }}</label>
+                                    <div class="progress mb-3">
+                                        <div class="progress-bar" role="progressbar"
+                                            aria-valuenow="{{ $link->percentage }}" aria-valuemin="0"
+                                            aria-valuemax="100">
+                                            <span class="pe-1 text-end">{{ $link->name }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+
                         </div>
-                    </div>
+                    @endif
+
                 </div>
-                <!---->
-                <!-- Laravel Skill -->
-                <div class="position-relative">
-                    <label class="progress-label">75</label>
-                    <div class="progress mb-3">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
-                            aria-valuemax="100">
-                            <span class="pe-1 text-end">Laravel</span>
-                        </div>
-                    </div>
-                </div>
-                <!---->
+
             </div>
-            <!---->
-        </div>
-        <!---->
-    </div>
-    <!---->
-</section>
+
+        </section>
+    @endif
+
+</div>
