d38607468d4847a99f4abe302fb11d803a422b11
Osama Abdelrahman <osbusiness662@gmail.com>
2025-11-13 03:28:57 +0000

    Add optional $class prop to def modal

--- resources/views/components/modal/def.blade.php
+++ resources/views/components/modal/def.blade.php
@@ -1,6 +1,6 @@
 <div wire:ignore.self class="modal fade" id="{{$id}}" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
     <div class="modal-dialog" role="document">
-        <div class="modal-content">
+        <div class="modal-content {{$class ?? ''}}">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalTitleId">{{ $title }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
