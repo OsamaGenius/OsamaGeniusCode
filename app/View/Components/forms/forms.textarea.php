01e37403ef134d56d92bf61d963ae006c94bc905
Osama Abdelrahman <osbusiness662@gmail.com>
2025-11-14 07:35:15 +0000

    Renamed and move components/forms.[textarea & descPreview] to components/forms/[textarea & descPreview]

--- app/View/Components/forms/forms.textarea.php
+++ app/View/Components/forms/forms.textarea.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class forms.textarea extends Component
+class textarea extends Component
 {
     /**
      * Create a new component instance.
