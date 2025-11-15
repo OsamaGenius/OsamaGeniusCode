67e4304fa5be73620819733d6748be2c7148ca46
Osama Abdelrahman <osbusiness662@gmail.com>
2025-11-15 08:14:56 +0000

    Making livewire modifier optional the default is "defer"

--- resources/views/components/forms/textarea.blade.php
+++ resources/views/components/forms/textarea.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-3">
-    <label for="" class="form-label"></label>
-    <textarea class="form-control" wire:model.defer="{{ $for }}" placeholder="{{ $placeholder }}"
+    <label for="{{$for}}" class="form-label"></label>
+    <textarea class="form-control" wire:model.{{$modifier ?? 'defer'}}="{{ $for }}" placeholder="{{ $placeholder }}"
         style="min-height: 100px"></textarea>
     {{$slot}}
 </div>
