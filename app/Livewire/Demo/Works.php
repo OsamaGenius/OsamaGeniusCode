38fb014da98f4a8af7c152bb5b239559fa464a86
Osama Abdelrahman <osbusiness662@gmail.com>
2025-11-26 16:52:42 +0000

    Activate search box operation

--- app/Livewire/Demo/Works.php
+++ app/Livewire/Demo/Works.php
@@ -4,11 +4,29 @@ namespace App\Livewire\Demo;
 
 use App\Models\Project;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class Works extends Component
 {
+
+    // Varients
+    public $search;
+    public $searchResult;
+    public $result = false;
+
     public function render()
     {
+
+        // Search Operation
+        if ($this->search !== '' && Str::length($this->search) > 0) {
+            $this->result = true;
+            $this->searchResult = Project::where('title', 'like', '%'.$this->search.'%')
+                                        ->orWhere('category', 'like', '%'.$this->search.'%')
+                                        ->get(['id', 'title', 'price', 'updated_at']);
+        } else {
+            $this->result = false;
+        }
+
         return view(
             'livewire.demo.works',
             [
--- resources/views/livewire/demo/works.blade.php
+++ resources/views/livewire/demo/works.blade.php
@@ -1,7 +1,14 @@
-<div>
+<div x-data="{
+    search: false,
+    result: @entangle('result')
+}">
+
     @if (count($templates) > 0 || count($projects) > 0 || count($photoshop) > 0)
+
         <section class="works py-5 fade-sections">
+
             <div class="container">
+
                 <!-- Section Title -->
                 <div class="title text-center mb-5">
                     <h3 class="mb-3">My Work - <span>My Latest Works</span></h3>
@@ -14,22 +21,65 @@
 
                 @if (Route::currentRouteName() !== 'homepage') {{-- All Works Page --}}
 
-                    <div class="text-end mb-3">
+                    <div class="text-end mb-3 position-relative">
+
+                        {{-- Filter by searching & select category --}}
                         <div class="btn-group">
 
-                            <a href="#" class="text-decoration-none me-3 mt-2">{{ __('All Works') }}</a>
-
-                            <select class="form-select s-150" name="" id="category">
+                            <select x-cloak x-show="search === false" x-transition class="form-select s-150"
+                                wire:model.live.throttle.150ms="category">
                                 <option value="" disabled>Select Category</option>
-                                <option value="1">Templates</option>
+                                <option value="*">All Works</option>
+                                <option value="Template">Templates</option>
+                                <option value="Project">Projects</option>
+                                <option value="photoshop">Photoshop</option>
                             </select>
 
                             <div class="btn-group">
-                                <input type="search" id="search" class="form-control ms-3 s-400 d-none"
+                                <input x-cloak x-show="search" x-transition type="search"
+                                    wire:model.live.throttle.100ms="search" class="form-control ms-3 s-400"
                                     placeholder="Search for templates, projects and more">
-                                <i class="fas fa-search pt-2 ms-3"></i>
+                                <i class="fas fa-search pt-2 ms-3" style="cursor: pointer"
+                                    x-on:click.prevent="search = !search"></i>
                             </div>
+
                         </div>
+
+                        <div class="position-absolute bg-white shadow-md rounded-3 p-3 mt-2 search-box" style="width: 500px" x-show="result" x-transition>
+
+                            @if ($searchResult)
+
+                                @if (count($searchResult) > 0)
+
+                                    @foreach ($searchResult as $i => $item)
+                                    
+                                        <div class="text-start mb-0" wire:key="{{$i}}">
+                                            <div class="d-flex">
+                                                <h5 class="mb-0">
+                                                    <a class="text-decoration-none"
+                                                        href="#">{{ $item->title }}</a>
+                                                </h5>
+                                                <p class="mb-0 text-end flex-grow-1">
+                                                    {{ $item->price > 0 ? '$' . $item->price : 'Free' }}</p>
+                                            </div>
+                                            <p class="mb-3">Project Bio</p>
+                                        </div>
+
+                                        @if ($i < count($searchResult) - 1)
+                                            <hr>                                            
+                                        @endif
+
+                                    @endforeach
+                                @else
+                                    <div class="alert alert-info text-center" role="alert">
+                                        No result founded for <strong>{{$search}}</strong> keyword
+                                    </div>
+                                @endif
+
+                            @endif
+
+                        </div>
+
                     </div>
 
                     {{-- Latest Templates --}}
@@ -51,6 +101,7 @@
                     @endif
                 @else
                     {{-- Homepage works --}}
+
                     {{-- Latest Templates --}}
                     @if (count($templates) > 0)
                         <h4 class="mb-3">{{ 'Latest ' . $templates[0]->category }}</h4>
@@ -68,18 +119,25 @@
                         <h4 class="mb-3">{{ 'Latest ' . $photoshop[0]->category }}</h4>
                         @livewire('demo.work.card', ['projects' => $photoshop], key($photoshop[0]->id))
                     @endif
+
                 @endif
 
             </div>
-            <!---->
+
         </section>
     @else
         @if (Route::currentRouteName() !== 'homepage')
             <div class="container py-5">
+
                 <div class="alert alert-info text-center" role="alert">
+
                     Their are no <strong>projects or templates</strong> in the warehouse yet!
+
                 </div>
+
             </div>
         @endif
+
     @endif
+
 </div>
