28823e025153058d6361e4167ca1cc08a5d0834c
Osama Abdelrahman <osbusiness662@gmail.com>
2025-12-01 16:22:04 +0000

    Implement about fetching data from db at public view

--- app/Livewire/Demo/About.php
+++ app/Livewire/Demo/About.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire\Demo;
 
+use App\Models\Info;
 use Livewire\Component;
 
 class About extends Component
 {
+
+    public $about;
+
     public function render()
     {
-        return view('livewire.demo.about');
+
+        $converter = app(\League\CommonMark\CommonMarkConverter::class);
+        
+        $data = Info::where('type', 'About')->orderby('created_at', 'ASC')->first(['title']);
+        
+        if ($data) 
+        {
+            $this->about = (string) $converter->convert($data->title);
+        } 
+        else 
+        {
+            $this->reset('about');
+        }
+    
+        return view(
+            'livewire.demo.about',
+        );
     }
 }
