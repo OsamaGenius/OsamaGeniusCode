4581d03e446bfb8150c288c8350f83e40b98886c
Osama Abdelrahman <osbusiness662@gmail.com>
2025-12-03 09:38:24 +0000

    Implement show/hide stories section in public view

--- app/Livewire/Demo/Story.php
+++ app/Livewire/Demo/Story.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire\Demo;
 
+use App\Models\Info;
 use Livewire\Component;
 
 class Story extends Component
 {
     public function render()
     {
-        return view('livewire.demo.story');
+        return view(
+            'livewire.demo.story',
+            [
+                'stories' => Info::where('type', 'story')->orderby('updated_at', 'desc')->limit('4')->get(['title', 'updated_at'])
+            ]
+        );
     }
 }
